2742c6094cce9bc7d21517733a0114366bd38999
John Preston <wcerfgba@riseup.net>
2016-07-10 16:11:29 +0100

    Add login route.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
@@ -11,9 +12,18 @@ class DefaultController extends Controller
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
-    {
+    public function indexAction(Request $request) {
       return $this->forward('AppBundle:ScoresGet:get', array(
         'n' => 10, 'name' => '', 'difficulty' => '', 'sort_field' => 'score' ));
+    }
+
+    /**
+     * @Route("/login", name="login")
+     * @Security("has_role('ROLE_ADMIN')")
+     */
+    public function loginAction(Request $request) {
+      $format = $request->getRequestFormat();
+
+      return $this->render("login.$format.twig");
     }
 }
